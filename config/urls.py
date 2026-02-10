616c8dd150607b75ebd371c74e10847c8013c769
bilalgull678 <bilalgull678@gmail.com>
2026-02-10 15:30:48 +0500

    cahnges

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("programs.urls")),
+    path('', RedirectView.as_view(url='/api/')),  # root URL redirect
 ]
